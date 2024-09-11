bbcaafdbc3be6fcaef32d4e3f25df558f97083c3
DeivMM <dmarquez@lortek.es>
2024-09-11 16:21:09 +0200

    20240911_cmmt_0

---
+++ new_mod.py
@@ -0,0 +1,5 @@
+def saludar():
+    print('Hola mundo')
+
+def sumar(a, b):
+    return a + b
---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="mi_modulo",
+    version="0.1",
+    packages=find_packages(),
+    description="Un módulo de ejemplo",
+    author="D. Márquez Monje",
+    author_email="d.marquez.monje@gmail.com",
+    url="https://github.com/deivMM/modulo_prueba",
+    install_requires=[],
+)
