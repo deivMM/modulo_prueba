910beafac9cd345962cc340eecc840c01871dad0
DeivMM <dmarquez@lortek.es>
2024-09-26 16:03:42 +0200

    20240926_cmmt_1

--- mi_modulo/new_mod.py
+++ mi_modulo/new_mod.py
@@ -2,7 +2,14 @@ def saludar():
     print('Hola mundo')
 
 def sumar(a, b):
-    res = a +b 
+    res = a + b 
+    if res > 100:
+        print(f'El resultado es mayor que 100: {res}')
+    else:
+        print(f'El resultado es menor que 100: {res}')
+
+def multiplicar(a, b):
+    res = a * b 
     if res > 100:
         print(f'El resultado es mayor que 100: {res}')
     else:
