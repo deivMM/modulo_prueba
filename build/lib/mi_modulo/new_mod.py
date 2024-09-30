5423be061d0bffd0de8c8619ec12e7992f26c41e
DeivMM <dmarquez@lortek.es>
2024-09-30 09:45:45 +0200

    20240930_cmmt_2

--- build/lib/mi_modulo/new_mod.py
+++ build/lib/mi_modulo/new_mod.py
@@ -14,3 +14,10 @@ def multiplicar(a, b):
         print(f'El resultado es mayor que 100: {res}')
     else:
         print(f'El resultado es menor que 100: {res}')
+
+def dividir(a, b):
+    res = a / b 
+    if res > 100:
+        print(f'El resultado es mayor que 100: {res}')
+    else:
+        print(f'El resultado es menor que 100: {res}')
